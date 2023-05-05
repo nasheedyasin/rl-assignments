8c231009a33d0ef9388cb2b04adf62572445ebd6
ijaj88 <112646666+ijaj88@users.noreply.github.com>
2023-05-04 21:38:23 -0400

    Updated

--- grp_prj/src/PPOImplementation.py
+++ grp_prj/src/PPOImplementation.py
@@ -23,8 +23,8 @@ class ActorNetwork(nn.Module):
     def __init__(self, inp_dim, out_dim):
         super(ActorNetwork, self).__init__()
         self.fc1 = nn.Linear(inp_dim, 64)
-        self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32, out_dim)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, out_dim)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -34,12 +34,13 @@ class ActorNetwork(nn.Module):
         action = dist.sample()
         log_prob = dist.log_prob(action)
         return action,log_prob,dist
+    
 class CriticNetwork(nn.Module):
     def __init__(self, inp_dim, out_dim):      
         super(CriticNetwork, self).__init__()
         self.fc1 = nn.Linear(inp_dim, 64)
-        self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32, out_dim)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, out_dim)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -58,31 +59,35 @@ class QLearningTrainer(object):
         self.values = []
         self.action=[]
         self.states=[]
+        self.terminate = []
         self.batchsize = batchsize
         self.updatesize= updatesize
-        self.eps_clip=0.8
+        self.eps_clip=0.2
+        self.maxtime = 500
+        self.interval = 4* self.maxtime
 
     def train(self):
+
+        global_time = 0
         
         for i in range(self.num_episodes):
-
-            modFlag = (i % self.batchsize)
-
-            if((modFlag ==0) & (i>0)):
-                self.update()        
-                self.rewards = []
-                self.log_probs = []
-                self.values = []
-                self.states=[]
-                self.action=[]
-
-
             env = self.env
             state, info = env.reset()
             done = False
             cumm=0
+            step =0
 
-            while not done:
+            # if((i>0)):
+            #     self.updateA2C()
+
+            #     self.rewards = []
+            #     self.log_probs = []
+            #     self.values = []
+            #     self.states=[]
+            #     self.action=[]
+            #     self.terminate=[]
+
+            while ((not done) and (step < self.maxtime)):
                 state = torch.from_numpy(state).float().unsqueeze(0).to(device)
                 self.states.append(state) 
                 action,log_prob,_ = modelActor(state)
@@ -93,8 +98,29 @@ class QLearningTrainer(object):
                 self.rewards.append(reward)
                 self.action.append(action)
 
+
+
                 done = terminated or truncated
                 cumm+= reward
+                step+=1
+                global_time+=1
+
+                self.terminate.append(done)
+
+                modFlag = (global_time % self.interval)
+
+
+                if((modFlag ==0) & (i>0)):
+                    self.update()
+
+                    self.rewards = []
+                    self.log_probs = []
+                    self.values = []
+                    self.states=[]
+                    self.action=[]
+                    self.terminate=[]
+
+
 
 
             reward_in_episodes.append(cumm)
@@ -105,7 +131,7 @@ class QLearningTrainer(object):
 
             if i % 50 == 0:
                 print(f"Episode {i},Rewards {cumm},mean {running_average}")
-            if (running_average > 200):
+            if (running_average > 480):
                 break
         
 
@@ -115,8 +141,11 @@ class QLearningTrainer(object):
             value_loss = []
             n_steps = len(self.rewards)
             QvalueMC = torch.empty(n_steps, dtype=torch.float).to(device)
-            Qvalue = 0
+
+            Qvalue=0
             for j in reversed(range(n_steps)):
+                if self.terminate[j]:
+                    Qvalue=0
                 Qvalue = self.rewards[j] + self.gamma * Qvalue
                 QvalueMC[j] = Qvalue
             mean_ = torch.mean((QvalueMC))
@@ -134,15 +163,19 @@ class QLearningTrainer(object):
                 _,_,dist = modelActor(states)
                 log_prob=dist.log_prob(actions)
                 value = modelCritic(states)
-                state_values = torch.squeeze(value)                
+                state_values = torch.squeeze(value) 
+                dist_entropy = dist.entropy()               
                 ratios = torch.exp(log_prob - log_probs_old.detach())
                 surr1 = ratios * advantage.detach()
                 surr2 = torch.clamp(ratios, 1-self.eps_clip, 1+self.eps_clip) * advantage.detach()              
-                policy_loss = torch.sum(-torch.min(surr1, surr2))
-                criterion = nn.SmoothL1Loss()
+                policy_loss = torch.sum((-torch.min(surr1, surr2)) - 0.0*dist_entropy )
+                criterion = nn.MSELoss()
+
                 value_loss = criterion(state_values, QvalueMC)
                 optimizerActor.zero_grad()
                 optimizerCritic.zero_grad()
+
+                #policy_loss = torch.sum(torch.stack(policy_loss))
                 value_loss.backward()
                 optimizerCritic.step()
                 policy_loss.backward()
@@ -159,6 +192,8 @@ class QLearningTrainer(object):
             for j in reversed(range(n_steps)):
                 Qvalue = self.rewards[j] + self.gamma * Qvalue
                 QvalueMC[j] = Qvalue
+
+
             mean_ = torch.mean((QvalueMC))
             std_ = torch.std(QvalueMC)
             if (std_!=0):
@@ -173,7 +208,7 @@ class QLearningTrainer(object):
             policy_loss = torch.sum(-log_probs * advantage.detach())
 
             # Critic loss
-            criterion = nn.SmoothL1Loss()
+            criterion = nn.MSELoss()
             value_loss = criterion(values, QvalueMC)
 
             optimizerActor.zero_grad()
@@ -196,7 +231,7 @@ modelCritic = CriticNetwork(env.observation_space.shape[0],1).to(device)
 optimizerCritic = optim.Adam(modelCritic.parameters(), lr=0.001)
 
 
-trainer = QLearningTrainer(env,2000,0.98,4,10)
+trainer = QLearningTrainer(env,2000,0.98,4,40)
 gamma = 0.98
 trainer.train()
 
